80166b06b3eb7aaaa73e3c24c1dc99e4fcc26b45
Asmoday4949 <malik.fleury@he-arc.ch>
2018-11-05 14:37:15 +0100

    Test of the implem.

--- Main.py
+++ Main.py
@@ -7,25 +7,26 @@ def heuristic_0(city_src, city_dest):
     return 0
 
 def heuristic_1(city_src, city_dest):
-    '''  '''
     return abs(city_src.get_x() - city_dest.get_x())
 
 def heuristic_2(city_src, city_dest):
-    return math.abs(city_src.get_y() - city_dest.get_y())
+    return abs(city_src.get_y() - city_dest.get_y())
 
 def heuristic_3(city_src, city_dest):
-    return math.sqrt(math.abs(city_src.get_x() - city_dest.get_x())**2 + math.abs(city_src.get_y() - city_dest.get_y())**2)
+    return sqrt(abs(city_src.get_x() - city_dest.get_x())**2 + abs(city_src.get_y() - city_dest.get_y())**2)
 
 def heuristic_4(city_src, city_dest):
-    return math.abs(city_src.get_x() - city_dest.get_x()) + math.abs(city_src.get_y() - city_dest.get_y())
+    return abs(city_src.get_x() - city_dest.get_x()) + abs(city_src.get_y() - city_dest.get_y())
 
 if __name__ == "__main__":
     graph = Graph()
     graph.debug_show()
+    # d = {"e":1, "b":5, "a":0}
+    # print(graph.min2(d))
 
-    # city_src = graph.get_city_from_name('Bern')
-    # city_dest = graph.get_city_from_name('Rome')
-    # result = graph.a_star(city_src, city_dest, heuristic_1)
-    #
-    # for city in result:
-    #     print(city.get_name())
+    city_src = graph.get_city_from_name('Bern')
+    city_dest = graph.get_city_from_name('Rome')
+    result = graph.a_star_en(city_src, city_dest, heuristic_4)
+
+    for city in result:
+        print(city.get_name())
